43035aebc6d8bb678a2587e51c41c9f39e1fd455
Renato Ceolin <renato.ceolin@renatoceolin.com>
2020-01-13 22:43:38 -0300

    Added number constraints for amount greater than 0 on transfer and withdraw schemas

--- lib/hiring_test_stone/transaction/transfer.ex
+++ lib/hiring_test_stone/transaction/transfer.ex
@@ -16,6 +16,7 @@ defmodule HiringTestStone.Transaction.Transfer do
     transfer
     |> cast(attrs, [:amount, :source_account_id, :destination_account_id])
     |> validate_required([:amount])
+    |> validate_number(:amount, greater_than: 0)
     |> foreign_key_constraint(:source_account_id)
     |> foreign_key_constraint(:destination_account_id)
   end
