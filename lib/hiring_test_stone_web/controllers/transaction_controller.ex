e24b127479d7e532c95cc01a3902267411648023
Renato Ceolin <renato.ceolin@renatoceolin.com>
2020-01-13 22:58:08 -0300

    Created controller actions to hold the transfer transaction functionality

--- lib/hiring_test_stone_web/controllers/transaction_controller.ex
+++ lib/hiring_test_stone_web/controllers/transaction_controller.ex
@@ -5,6 +5,7 @@ defmodule HiringTestStoneWeb.TransactionController do
   alias HiringTestStoneWeb.ErrorView
 
   @transaction_withdraw "withdraw"
+  @transaction_transfer "transfer"
 
   def create(conn, %{"transaction_type" => @transaction_withdraw} = params) do
     case Transaction.withdraw_changeset(params) do
@@ -19,12 +20,54 @@ defmodule HiringTestStoneWeb.TransactionController do
     end
   end
 
+  def create(conn, %{"transaction_type" => @transaction_transfer} = params) do
+    case Transaction.transfer_changeset(params) do
+      %{valid?: false} = changeset ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> put_view(ErrorView)
+        |> render("422.json", %{changeset: changeset})
+      _ = changeset ->
+        conn
+        |> perform_transfer(changeset)
+    end
+  end
+
+  def create(conn, params) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(ErrorView)
+    |> render("422.json", %{changeset: Transaction.withdraw_changeset(params)})
+  end
+
   defp perform_withdraw(conn, %Ecto.Changeset{changes: %{source_account: source_account_number, amount: amount}}) do
     case Transaction.withdraw_money(source_account_number, amount) do
       {:ok, %{register_withdraw_transaction_step: transaction}} ->
         conn
         |> put_status(:created)
         |> render("create.json", transaction: transaction, type: @transaction_withdraw)
+      {:error, transaction_step, error, _} ->
+        conn
+        |> put_status(:bad_request)
+        |> render("400.json", %{error: %{transaction_step: transaction_step, error: error}})
+    end
+  end
+
+  defp perform_transfer(
+    conn,
+    %Ecto.Changeset{
+      changes: %{
+        source_account: source_account_number,
+        destination_account: destination_account_number,
+        amount: amount
+      }
+    }
+  ) do
+    case Transaction.transfer_money(source_account_number, destination_account_number, amount) do
+      {:ok, %{register_transfer_transaction_step: transaction}} ->
+        conn
+        |> put_status(:created)
+        |> render("create.json", transaction: transaction, type: @transaction_transfer)
       {:error, transaction_step, error, _} ->
         conn
         |> put_status(:bad_request)
