a925a6ff757be26703b9dcec51f64189ab2ae67d
Renato Ceolin <renato.ceolin@renatoceolin.com>
2020-01-18 18:15:49 -0300

    renanming modulee

--- config/releases.exs
+++ config/releases.exs
@@ -1,4 +1,4 @@
-config :hiring_test_stone, GigalixirGettingStartedWeb.Endpoint,
+config :hiring_test_stone, HiringTestStoneWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
