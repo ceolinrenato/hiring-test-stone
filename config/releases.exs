b467545de736a985714629a14f9850f1508e1f6e
Renato Ceolin <renato.ceolin@renatoceolin.com>
2020-01-18 18:48:43 -0300

    Using gigalixir config only when deploying to gigalixir

--- config/releases.exs
+++ config/releases.exs
@@ -1,6 +1,8 @@
 import Config
 
-config :hiring_test_stone, HiringTestStoneWeb.Endpoint,
-  server: true,
-  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+if System.get_env("APP_NAME") do
+  config :hiring_test_stone, HiringTestStoneWeb.Endpoint,
+    server: true,
+    http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+    url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+end
